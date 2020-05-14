b3f5cebc1f1e1c03a649cd0585a9a9da2eaf8d93
Matthew Leibowitz <mattleibow@live.com>
2020-05-14 17:35:37 +0200

    Update Program.cs

--- XamarinGitHubActions/Program.cs
+++ XamarinGitHubActions/Program.cs
@@ -6,7 +6,7 @@ namespace XamarinGitHubActions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub Actions World!");
         }
     }
 }
