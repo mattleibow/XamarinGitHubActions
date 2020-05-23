b343aac19167d660c7636a843efa11e8b9a8e989
Matthew Leibowitz <mattleibow@live.com>
2020-05-23 19:46:55 +0200

    Added initial library build

---
+++ Library/Library.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Library.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var c = new Class1();
+        }
+    }
+}
